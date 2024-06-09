f379e6d39fec13eb7a508750d2dca7859dfe9574
Neel Doshi <neeldoshi147@gmail.com>
2024-06-10 02:26:10 +0530

    Day - 1 - `100DaysOfSwift`

---
+++ Day1/variables, simple data types, and string interpolation.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+// Variables are mutuable whereas let is constant immutable
+var greeting = "Hello, playground"
+
+var name = "abc"
+print(name)
+name = "Neel" //changed
+
+let baseurl = "www.google.com" //constant like const in kt
+//baseurl = "some url" // Throws error
+
+var strr :UInt = 12345
+
+// Swift Data Types : characters , String, Int, Float, Double, Bool
+
+// String Interpolation
+
+print("My name is \(name)")
+
