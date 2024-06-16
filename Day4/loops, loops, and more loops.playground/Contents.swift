111a3e29c20d48579d0004e12daa822fe796fadc
Neel Doshi <neeldoshi147@gmail.com>
2024-06-16 18:59:58 +0530

    Day 4 – loops, loops, and more loops

---
+++ Day4/loops, loops, and more loops.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+// For loops
+let count = 1...10
+
+print(count)
+for i in count{
+    print(i)
+}
+
+// Array Python Style
+
+let albums = ["Red", "1989", "Reputation"]
+
+for album in albums {
+    print("\(album) is on Apple Music")
+}
+
+// While loop same as other lang
+
+
+// Repeat --> do while
+var number = 1
+
+repeat {
+    print(number)
+    number += 1
+} while number <= 20
+
+print("Ready or not, here I come!")
